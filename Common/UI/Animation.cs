eac93c29a4b671505a06b0039178ea3a1d82ae43
Elvyria <elvyria@tutanota.com>
2023-03-02 04:07:54 +0300

    Added core static class for animations

---
+++ Common/UI/Animation.cs
@@ -0,0 +1,24 @@
+using Microsoft.Xna.Framework;
+
+namespace MagicStorage.Common.UI;
+
+public static class Animation
+{
+	public static float Ease(float from, float to, float speed, ref float time)
+	{
+		const float limit = 100f;
+
+		float sign = unchecked((float)(1 - (0xFFFFFE & (uint) (to - from) >> 31)));
+
+		time += sign * speed * (limit - time + 2);
+
+		return MathHelper.Lerp(from, to, time / (limit + 2));
+	}
+
+	public static float Ease(float from, float to, float speed, float time)
+	{
+		const float limit = 100f;
+
+		return MathHelper.Lerp(from, to, time / (limit + 2));
+	}
+}
